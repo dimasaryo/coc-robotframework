9bc37f3b28604b01582478f5270b57609626aaad
Dimas Aryo <dimas.aryo@ruangguru.com>
2020-10-31 22:54:07 +0700

    Add loading status

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,20 @@ import {
 } from 'coc.nvim'
 
 export async function activate(context: ExtensionContext): Promise<void> {
+    let statusItem = workspace.createStatusBarItem(0, { progress: true})
+    statusItem.text = 'loading robot framework extension.'
+    statusItem.show()
+    try {
+        let res = await activateUnsafe(context)
+        statusItem.dispose()
+        return res
+    } catch (ex) {
+        statusItem.dispose()
+        throw ex
+    }
+}
+
+export async function activateUnsafe(context: ExtensionContext): Promise<void> {
     const config = workspace.getConfiguration('coc-robotframework')
     const isEnable = config.get<boolean>('enable', true)
     if (!isEnable) {
